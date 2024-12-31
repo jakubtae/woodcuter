5cffc3ed4f7d0c85f2291b36f0e0c658cbe85fa1
Jakub <jakub.tomczyk.2005@gmail.com>
2024-12-31 17:18:09 +0100

    shadcn select

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,13 @@ import {
   MaterialGroupType,
   materials,
 } from "@/lib/materials";
-
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 export default function Home() {
   // State to store the selected type and board_name
   const [selectedType, setSelectedType] = useState<string>(
@@ -47,64 +53,64 @@ export default function Home() {
   return (
     <div className="flex flex-col items-start justify-items-center min-h-screen p-8 pb-20 gap-2 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       {/* Select Type */}
-      <div>
-        <label htmlFor="type-select">Choose a Type:</label>
-        <select
-          id="type-select"
-          value={selectedType}
-          onChange={handleTypeChange}
-        >
-          {groupedMaterials.map((group: MaterialGroupType, index) => (
-            <option key={index} value={group.type}>
-              {group.type}
-            </option>
-          ))}
-        </select>
-      </div>
-
-      {/* Select Board Name based on Type */}
-      {selectedType && (
+      <div className="bg-violet-300">
         <div>
-          <label htmlFor="board-name-select">Choose a Board Name:</label>
-          <select
-            id="board-name-select"
-            value={selectedBoardName}
-            onChange={handleBoardNameChange}
-          >
-            {groupedMaterials
-              .find((group) => group.type === selectedType)
-              ?.items.map((item: Material, index) => (
-                <option key={index} value={item.board_name}>
-                  {item.board_name}
-                </option>
+          <Select value={selectedType} onValueChange={setSelectedType}>
+            <SelectTrigger className="w-[280px]">
+              <SelectValue placeholder="Wybierz rodzaj :" />
+            </SelectTrigger>
+            <SelectContent>
+              {groupedMaterials.map((group: MaterialGroupType, index) => (
+                <SelectItem key={index} value={group.type}>
+                  {group.type}
+                </SelectItem>
               ))}
-          </select>
+            </SelectContent>
+          </Select>
         </div>
-      )}
-
-      {/* Display selected values */}
-
-      {/* Display details of the selected material */}
-      {selectedMaterial && (
-        <div className="mt-6 p-4 border rounded-lg bg-gray-100">
-          <h3>Material Details</h3>
-          <p>
-            <strong>Board Name:</strong> {selectedMaterial.board_name}
-          </p>
-          <p>
-            <strong>Thickness:</strong> {selectedMaterial.thickness} mm
-          </p>
-          <p>
-            <strong>Width:</strong> {selectedMaterial.width} mm
-          </p>
-          <p>
-            <strong>Height:</strong> {selectedMaterial.height} mm
-          </p>
-          <p>
-            <strong>Quantity:</strong> {selectedMaterial.quantity}
-          </p>
-        </div>
-      )}
+        {/* Select Board Name based on Type */}
+        {selectedType && (
+          <Select
+            value={selectedBoardName}
+            onValueChange={setSelectedBoardName}
+          >
+            <SelectTrigger className="w-[280px]">
+              <SelectValue placeholder="Wybierz płytę :" />
+            </SelectTrigger>
+            <SelectContent>
+              {groupedMaterials
+                .find((group) => group.type === selectedType)
+                ?.items.map((item: Material, index) => (
+                  <SelectItem key={index} value={item.board_name}>
+                    {item.board_name}
+                  </SelectItem>
+                ))}
+            </SelectContent>
+          </Select>
+        )}
+        {/* Display selected values */}
+        {/* Display details of the selected material */}
+        {selectedMaterial && (
+          <div className="mt-6 p-4 border rounded-lg bg-gray-100 w-full">
+            <h3>Material Details</h3>
+            <p>
+              <strong>Board Name:</strong> {selectedMaterial.board_name}
+            </p>
+            <p>
+              <strong>Thickness:</strong> {selectedMaterial.thickness} mm
+            </p>
+            <p>
+              <strong>Width:</strong> {selectedMaterial.width} mm
+            </p>
+            <p>
+              <strong>Height:</strong> {selectedMaterial.height} mm
+            </p>
+            <p>
+              <strong>Quantity:</strong> {selectedMaterial.quantity}
+            </p>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
